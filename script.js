7268a9743d768567db8cf94af1a10a9aa3a6423b
Parag Shirsat <90747323+ParagShirsat@users.noreply.github.com>
2022-10-05 12:20:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+console.log("Welcome to Spotify");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('C:/Users/PARAG/Desktop/project/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    {songName: "1.Beliver", filePath:"C:/Users/PARAG/Desktop/project/1.mp3"},
+    {songName: "2.Champion - Dwayne DJ Bravo", filePath: "C:/Users/PARAG/Desktop/project/2.mp3"},
+    {songName: "3.Muqabla - Street Dancer 3D",filePath:"C:/Users/PARAG/Desktop/project/3.mp3"},
+    {songName: "4.ICC Mens T20 World Cup 2021 Official Anthem - Live The Game", filePath: "C:/Users/PARAG/Desktop/project/4.mp3" },
+    {songName: "5.Elektronomia_Sky_High", filePath: "C:/Users/PARAG/Desktop/project/5.mp3"},
+    {songName: "6.Coca Cola - Luka Chuppi", filePath: "C:/Users/PARAG/Desktop/project/6.mp3"},
+]
+
+songItems.forEach((element, i)=>{ 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+ 
+
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = 'C:/Users/PARAG/Desktop/project/${songIndex+1}.mp3';
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=7){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src ='C:/Users/PARAG/Desktop/project/${songIndex+1}.mp3';
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = 'C:/Users/PARAG/Desktop/project/${songIndex+1}.mp3';
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
